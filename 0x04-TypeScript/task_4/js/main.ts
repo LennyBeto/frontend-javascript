21c3c5fcae5466c70b63955351329d6895e0ab45
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-11-16 21:55:29 +0300

    Update main.ts

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -11,25 +11,25 @@ export const react = new Subjects.React();
 
 // Export teacher object
 export const cTeacher: Subjects.Teacher = {
-  firstName: "John",
-  lastName: "Doe",
+  firstName: 'John',
+  lastName: 'Doe',
   experienceTeachingC: 10,
 };
 
 // C++
-console.log("C++"); // Output: C++
+console.log('C++'); // Output: C++
 cpp.setTeacher(cTeacher);
 console.log(cpp.getRequirements()); // output: Here is the list of requirements for Cpp
 console.log(cpp.getAvailableTeacher()); // OutputAvailable Teacher: John
 
 // Java
-console.log("Java"); // Output: Java
+console.log('Java'); // Output: Java
 java.setTeacher(cTeacher);
 console.log(java.getRequirements()); // Here is the list of requirements for Java
 console.log(java.getAvailableTeacher()); // No available teacher
 
 // React
-console.log("React"); // Output: React
+console.log('React'); // Output: React
 react.setTeacher(cTeacher);
 console.log(react.getRequirements()); // Here is the list of requirements for React
 console.log(react.getAvailableTeacher()); // No available teacher
